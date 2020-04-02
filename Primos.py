a69fa2282edddede64611ba7b89022043fef3972
victortarriga <victor.tarriga@gmail.com>
2020-04-02 18:37:00 -0300

    pqp

--- Primos.py
+++ Primos.py
@@ -1,9 +1,22 @@
-primos = []
-for num in range(1,101):
-    count = 0
-    for numb in range(1,num+1):
-        if num % numb == 0:
-            count += 1
-    if count ==2:
-        primos.append (num)
-print (primos)
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def primoss():
+    primos = ""
+    for num in range(1,548):
+        count = 0
+        for numb in range(1,num+1):
+            if num % numb == 0:     
+                count += 1
+        if count ==2:        
+            primos += str(num) + ', '   
+    return (primos [0:len(primos)-2])
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
