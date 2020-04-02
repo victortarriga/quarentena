6a5910add5901835ecaf4de39290c2b1fbf96841
victortarriga <victor.tarriga@gmail.com>
2020-04-02 18:47:07 -0300

    porralouca

--- Primos.py
+++ Primos.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 def primoss():
     primos = ""
-    for num in range(1,548):
+    for num in range(1,548):  #contador dos 100 primeiros números primos!
         count = 0
-        for numb in range(1,num+1):
-            if num % numb == 0:     
+        for numb in range(1,num+1):  #aqui é pra ver quantas vezes é dividido até chegar nele mesmo.
+            if num % numb == 0:     # se ele for divisivel conta 1
                 count += 1
-        if count ==2:        
+        if count ==2:        #pra um número ser primo, só pode ser divisel 2x
             primos += str(num) + ', '   
     return (primos [0:len(primos)-2])
 
